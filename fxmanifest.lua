25bec342084df7c81ee40dfc66872e0ee6987bab
adminmohammed <105809983+adminmohammed@users.noreply.github.com>
2025-08-05 01:41:13 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,35 @@
 fx_version 'bodacious'
 games { 'gta5' }
+lua54 "yes"
 
-client_scripts {'lib/Proxy.lua','lib/Tunnel.lua','Config.lua','client.lua'}
-server_scripts {'utils.lua','lib/MySQL.lua','Config.lua','server.lua'}
+
+name 'TE-Garage'
+version 'v1.2.1'
+author 'ARMANDO#8257'
+
+escrow_ignore {
+
+  "Config.lua",
+}
+
+client_scripts {
+    'lib/Proxy.lua',
+    'lib/Tunnel.lua',
+    'Config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'utils.lua',
+    'lib/MySQL.lua',
+    'Config.lua',
+    'server.lua'
+}
+
 ui_page_preload 'yes'
+
 ui_page 'FrontEnd/index.html'
-files {'FrontEnd/*.*',}
+
+files {
+    'FrontEnd/*.*',
+}
